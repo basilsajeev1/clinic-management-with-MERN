ec3a054ae3455b3b838ce1654e8e86dabe9f80a8
Basil Sajeev <basilsajeev1@gmail.com>
2022-09-02 18:00:27 +0530

    first commit on front end

---
+++ front-end/src/pages/signup/signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+import Signup from '../../components/signup/Signup';
+
+function SignupPage() {
+  return (
+    <div>
+      <Header/>
+      <Signup />
+    </div>
+  );
+}
+
+export default SignupPage;
